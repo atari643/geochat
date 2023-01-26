f76b36cc7183fd3ce7889befea2c5205400accbe
Grégoire Passault <g.passault@gmail.com>
2023-01-26 16:24:47 +0100

    Switching lat/lng to lng/lat

--- src/DataFixtures/GeochatMessagesFixtures.php
+++ src/DataFixtures/GeochatMessagesFixtures.php
@@ -11,14 +11,14 @@ class GeochatMessagesFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $messages = [
-            [44.8451032, -0.573892, 'Super la #foire place des quinconces!'],
-            [44.7913014, -0.6116024, 'Un bonjour du département #iutinformatique'],
-            [44.830072, -0.712858, "Je viens d'arriver à #aeoroportDeBordeaux, j'arrive en ville!"],
-            [44.8942553, -0.5684104, "Bientôt le #BordeauxGeekFest, venez nombreux!"],
-            [44.8293753, -0.6020268, "Prenez soin de vous et des autres, donnez votre sang"]
+            [-0.573892, 44.8451032, 'Super la #foire place des quinconces!'],
+            [-0.6116024, 44.7913014, 'Un bonjour du département #iutinformatique'],
+            [-0.712858, 44.830072, "Je viens d'arriver à #aeoroportDeBordeaux, j'arrive en ville!"],
+            [-0.5684104, 44.8942553, "Bientôt le #BordeauxGeekFest, venez nombreux!"],
+            [-0.6020268, 44.8293753, "Prenez soin de vous et des autres, donnez votre sang"]
         ];
 
-        foreach ($messages as list($lat, $lng, $text)) {
+        foreach ($messages as list($lng, $lat, $text)) {
             $message = new Message;
             $message
                 ->setText($text)
